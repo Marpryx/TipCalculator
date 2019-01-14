3ec364d940c85f88853e44c67fe7ed4e392ffc20
Marpryx <pritismar@hotmail.com>
2019-01-14 16:03:25 -0500

    Added comments and validation

--- tipsCalc.js
+++ tipsCalc.js
@@ -7,12 +7,12 @@ function calculateTip() {
     var service = document.getElementById("service").value;
     var friendsAmt = document.getElementById("friendsAmt").value;
 
-  //validate input
-    if (billAmt === "" || service === 0) {
-        alert("Please enter the value");
+  //validate input not null and number (isNan)
+    if (billAmt === "" || service === 0 || isNaN(billAmt) || isNaN(friendsAmt)) {
+        alert("Please enter the only positive numbers!");
         return;
     }
-  //Check to see if this input is empty or less than or equal to 1
+  //Check the amount of friends and according to the number it will show or block text "each"
     if (friendsAmt === "" || friendsAmt <= 1) {
         friendsAmt = 1;
         document.getElementById("each1").style.display = "none";
@@ -22,10 +22,10 @@ function calculateTip() {
         document.getElementById("each2").style.display = "block";
     }
 
-  //Calculate tip
+  //Calculate the amount of tip
     var totalTip = (billAmt * service) / friendsAmt;
 
-    //Calculate Total Amount
+    //Calculate the total amount to be paid
     var totalAmount = (billAmt / friendsAmt) + totalTip;
     //round to two decimal places
     totalAmount = Math.round(totalAmount * 100) / 100;
@@ -38,21 +38,18 @@ function calculateTip() {
     totalTip = totalTip.toFixed(2);
 
 
-  //Display the tip
+  //Display the tip in the form
     document.getElementById("totalTip").style.display = "block";
     document.getElementById("tip").innerHTML = totalTip;
 
-    //Display the Total Amount
+    //Display the Total Amount in the form
     document.getElementById("totalAmt").style.display = "block";
     document.getElementById("amtPlusTip").innerHTML = totalAmount;
 
 }
 
-//Hide the tip amount on load
-//document.getElementById("totalTip").style.display = "none";
-//document.getElementById("each1").style.display = "none";
 
-//click to call function
+//Call the function to be performed
 document.getElementById("calculateTip").onclick = function () {
     "use strict";
     calculateTip();
